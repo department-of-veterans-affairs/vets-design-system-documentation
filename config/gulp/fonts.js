1668bf65048e4e6a22270569a303577c33f5414b
Jared Cunha <jcunha@omb.eop.gov>
2018-10-15 16:20:12 -0400

    asset 404 fix

--- config/gulp/fonts.js
+++ config/gulp/fonts.js
@@ -1,16 +1,6 @@
 var gulp = require('gulp');
 var task = 'fonts';
 
-gulp.task(task, function (done) {
-  console.log("Moving fonts into folder");
-  var stream = gulp.src('./tmp_remote_assets/public/fonts/*')
-    .pipe(gulp.dest('src/vendor/fonts'));
-
-  return stream;
-
-});
-
-
 
 gulp.task('move-formation-fonts', function (done) {
   console.log("Moving fonts into folder");
@@ -29,5 +19,17 @@ gulp.task('move-fontawesome-fonts', function (done) {
   return stream;
 });
 
+gulp.task('move-bitter-fonts', function (done) {
+  console.log("Moving Fontawesome into folder");
+  var stream = gulp.src('./tmp_remote_assets/public/fonts/bitter*')
+    .pipe(gulp.dest('src/fonts'));
 
-gulp.task(task, gulp.series('move-formation-fonts', 'move-fontawesome-fonts'));
+  return stream;
+});
+
+
+gulp.task(task, gulp.series(
+  'move-formation-fonts',
+  'move-fontawesome-fonts',
+  'move-bitter-fonts'
+));
