2e1a1606ddfdb9796a1a64549fbbbfc535f5ecca
Lisa Chung <lisac.usds@gmail.com>
2018-10-16 06:26:07 -0400

    add sample react component

---
+++ webpack/components/myReactComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MyReactComponent extends Component {
+ render() {
+ return (
+ <div><h3>This is a React component!</h3></div>
+ )
+ }
+}
+export default MyReactComponent;
