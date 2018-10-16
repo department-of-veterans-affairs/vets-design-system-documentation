3d7967aa71bdcb99a8821e34e13a4a175556242b
Lisa Chung <lisac.usds@gmail.com>
2018-10-16 06:54:03 -0400

    add detail to the sample react component - display a datepicker and spinner

--- webpack/components/myReactComponent.js
+++ webpack/components/myReactComponent.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+
+
+var Loader = require('halogen/PulseLoader');
 
 class MyReactComponent extends Component {
  render() {
  return (
- <div><h3>This is a React component!</h3></div>
+ <div>
+ 
+ <h3>This is a React component!</h3>
+ 
+ <div id='sampleDatepicker'>
+ 	This is a datepicker:
+ 	<DatePicker showTimeSelect />
+ </div>
+
+<div id='sampleLoader'>
+	This is a loading spinner:
+	<Loader color="#AAA" size="50px" margin="4px"/>
+	<a href="https://github.com/yuanyan/halogen">halogen on github</a>
+</div>
+
+ </div>
+
+
+
  )
  }
 }
