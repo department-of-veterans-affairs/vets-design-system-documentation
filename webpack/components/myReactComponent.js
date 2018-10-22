9052f91270f2c800bd11f041bc3db43ea8a1baf8
Jared Cunha <jcunha@omb.eop.gov>
2018-10-22 15:39:17 -0400

    version bump and whatnot

--- webpack/components/myReactComponent.js
+++ webpack/components/myReactComponent.js
@@ -1,12 +1,4 @@
 import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
-
-import 'react-datepicker/dist/react-datepicker.css';
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
-
-
-var Loader = require('halogen/PulseLoader');
 
 class MyReactComponent extends Component {
  render() {
@@ -14,17 +6,6 @@ class MyReactComponent extends Component {
  <div>
 
  <h3>This is a React component!</h3>
-
- <div id='sampleDatepicker'>
-   This is a datepicker:
-   <DatePicker showTimeSelect />
- </div>
-
-<div id='sampleLoader'>
-  This is a loading spinner:
-  <Loader color="#AAA" size="50px" margin="4px"/>
-  <a href="https://github.com/yuanyan/halogen">halogen on github</a>
-</div>
 
 
  </div>
