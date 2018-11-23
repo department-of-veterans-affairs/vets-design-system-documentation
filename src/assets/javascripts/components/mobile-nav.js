7db01d4ef401dce40844f00c33f2eb00ce87a73b
Jared Cunha <jcunha@omb.eop.gov>
2018-11-23 15:46:35 -0500

    nav overlay

--- src/assets/javascripts/components/mobile-nav.js
+++ src/assets/javascripts/components/mobile-nav.js
@@ -2,14 +2,35 @@
 var close_mobile_nav_button = document.getElementById("close_mobile_nav_button"),
     open_mobile_nav_button = document.getElementById("open-mobile-nav-button"),
     mobile_nav = document.getElementById("mobile-nav"),
-    visible_class = "is-visible";
+    overlay = document.createElement("div");
+    nav_open_class = "nav-is-open",
+    visible_class = "is-visible",
+    overlay_class = "site-c-overlay";
+
+
+function removeElementsByClass(className){
+  var elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+  }
+}
 
 close_mobile_nav_button.addEventListener("click", function(e) {
+  document.body.classList.remove(nav_open_class);
   mobile_nav.classList.remove(visible_class);
-  open_mobile_nav_button.focus()
+  open_mobile_nav_button.focus();
+  removeElementsByClass(overlay_class);
 });
 
 open_mobile_nav_button.addEventListener("click", function(e) {
+  document.body.classList.add(nav_open_class);
   mobile_nav.classList.add(visible_class);
   close_mobile_nav_button.focus();
+  document.body.insertBefore(overlay, mobile_nav);
+  overlay.classList.add(overlay_class);
+
+  setTimeout(function(){
+    overlay.classList.add(visible_class);
+  }, 100);
+
 })
