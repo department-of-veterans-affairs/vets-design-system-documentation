59587aa08007617161e9b2c7b1f625756d1eae84
Jared Cunha <jcunha@omb.eop.gov>
2019-03-29 13:57:02 -0400

    mobile seach

--- src/assets/javascripts/site.js
+++ src/assets/javascripts/site.js
@@ -1,7 +1,12 @@
 ---
 # Javascipts!!!
 ---
+
+{% include_relative functions/breakpoints.js %}
+{% include_relative vendor/search.js %}
+
 {% include_relative components/anchors.js %}
 {% include_relative components/mobile-nav.js %}
 {% include_relative components/iframe-preview.js %}
 {% include_relative components/search.js %}
+
