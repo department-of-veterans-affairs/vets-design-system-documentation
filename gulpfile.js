b6f51665edc59af618e0c9bf3cb29774411efd39
Jared Cunha <jcunha@omb.eop.gov>
2019-04-05 16:06:47 -0400

    update formation and footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 // Bring in individual Gulp configurations
 //
 
-require( './config/gulp/remote-assets' );
 require( './config/gulp/fonts' );
 require( './config/gulp/images' );
 require( './config/gulp/css' );
 require( './config/gulp/javascript' );
-require( './config/gulp/paths' );
 require( './config/gulp/build' );
 
 var gulp = require( 'gulp' );
