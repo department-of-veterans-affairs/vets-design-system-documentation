d97e883f8a63414d7936f933ba9fd61440ebf8cf
Jared Cunha <jcunha@omb.eop.gov>
2019-04-25 13:49:42 -0400

    support 6.2.1

--- config/gulp/javascript.js
+++ config/gulp/javascript.js
@@ -1,16 +1,6 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 
-
-gulp.task('copy-uswds-js', function (done) {
-  console.log('copying uswds javascript');
-  var stream = gulp.src('./node_modules/uswds/dist/js/**/*')
-    .pipe(gulp.dest('src/vendor/javascripts'));
-
-  return stream;
-});
-
-
 gulp.task('copy-react-js', function (done) {
   console.log('copying uswds javascript');
   var stream = gulp.src('./node_modules/formation/src/components/**/*.njk')
@@ -30,4 +20,4 @@ gulp.task('copy-formation-js', function (done) {
 });
 
 
-gulp.task('javascript', gulp.series('copy-uswds-js', 'copy-react-js'));
+gulp.task('javascript', gulp.series('copy-formation-js', 'copy-react-js'));
