10f1b0fee2a992b6ccccfbc742bec80fd8f65bbd
Katryn Bowman <katy.bowman@adhocteam.us>
2021-12-03 10:14:22 -0500

    upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loader: "babel-loader",
-        query: {
+        options: {
           presets: ["react", "es2015"]
         }
       }
