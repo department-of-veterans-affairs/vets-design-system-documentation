6bef9bac8caef46631793ac483330ec9711b244c
Brooks Johnson <brooks.johnson@pm.me>
2022-05-05 16:34:53 -0700

    Move npm files w/ gulp and load on page

--- config/gulp/javascript.js
+++ config/gulp/javascript.js
@@ -19,5 +19,13 @@ gulp.task('copy-formation-js', function (done) {
   return stream;
 });
 
+gulp.task('copy-web-components-js', function (done) {
+  console.log('copying web-components javascript');
+  var stream = gulp.src(
+    './node_modules/@department-of-veterans-affairs/web-components/**/*.js')
+    .pipe(gulp.dest('src/vendor/javascripts/web-components/'));
 
-gulp.task('javascript', gulp.series('copy-formation-js', 'copy-react-js'));
+  return stream;
+});
+
+gulp.task('javascript', gulp.series('copy-formation-js', 'copy-web-components-js'));
