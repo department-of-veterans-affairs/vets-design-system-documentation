0725b269e2b147289fda24a684622c163cec53bc
Brooks Johnson <brooks.johnson@pm.me>
2022-05-05 16:52:58 -0700

    Update output path to component-library
    This should make things easier once the web-components package gets
    promoted to be the main component-library package

--- config/gulp/javascript.js
+++ config/gulp/javascript.js
@@ -14,7 +14,7 @@ gulp.task('copy-web-components-js', function (done) {
   console.log('copying web-components javascript');
   var stream = gulp.src(
     './node_modules/@department-of-veterans-affairs/web-components/**/*.js')
-    .pipe(gulp.dest('src/vendor/javascripts/web-components/'));
+    .pipe(gulp.dest('src/vendor/javascripts/component-library/'));
 
   return stream;
 });
